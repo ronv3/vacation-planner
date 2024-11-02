4a95ceb7240e623de827a0d2825ce046340271cf
ronv3 <aaron.vahtra@gmail.com>
2024-11-02 17:32:02 +0200

    Corrected data fetch, removed traces of hibernate function

--- vacation-service/src/main/java/com/kodality/vacation/request/VacationRequestRepository.java
+++ vacation-service/src/main/java/com/kodality/vacation/request/VacationRequestRepository.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,19 +19,23 @@ public class VacationRequestRepository {
     this.jdbcTemplate = jdbcTemplate;
   }
 
+  // SQL query
   public List<VacationRequest> getVacationRequests() {
     String sql = "SELECT id, employee_name, submitted_at, comment FROM vacation_request";
     return jdbcTemplate.query(sql, new VacationRequestRowMapper());
   }
 
+  // Row mapper - convert data from a database to java object.
   private static class VacationRequestRowMapper implements RowMapper<VacationRequest> {
     @Override
     public VacationRequest mapRow(ResultSet rs, int rowNum) throws SQLException {
       return new VacationRequest()
               .setId(rs.getLong("id"))
               .setEmployeeName(rs.getString("employee_name"))
-              .setSubmittedAt(rs.getObject("submitted_at", LocalDateTime.class))
-              .setComment(rs.getString("comment"));
+              .setVacationStart(rs.getObject("vacation_start", LocalDate.class))
+              .setVacationEnd(rs.getObject("vacation_end", LocalDate.class))
+              .setComment(rs.getString("comment"))
+              .setSubmittedAt(rs.getObject("submitted_at", LocalDateTime.class));
     }
   }
 }
