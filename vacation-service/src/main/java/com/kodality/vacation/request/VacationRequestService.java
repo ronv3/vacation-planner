48665aaa374d3ef6d247df84334863a192a73db0
ronv3 <aaron.vahtra@gmail.com>
2024-11-02 15:58:47 +0200

    Integrated Micronaut, liquibase.

--- vacation-service/src/main/java/com/kodality/vacation/request/VacationRequestService.java
+++ vacation-service/src/main/java/com/kodality/vacation/request/VacationRequestService.java
@@ -1,9 +1,13 @@
 package com.kodality.vacation.request;
 
+import io.micronaut.context.annotation.Requires;
 import jakarta.inject.Singleton;
+
+import javax.sql.DataSource;
 import java.util.List;
 
 @Singleton
+@Requires(beans = DataSource.class)
 public class VacationRequestService {
 
   private final VacationRequestRepository vacationRequestRepository;
